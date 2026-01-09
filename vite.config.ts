ce4a091f8191f9af0e520a70e2ef152e0afd3d4f
Yuto Tejima <>
2026-01-09 18:03:19 +0900

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { cloudflare } from "@cloudflare/vite-plugin"
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		vue(),
-		vueDevTools(),
-		cloudflare()
-	],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		},
-	},
-})
+export default defineConfig(async ({ mode }) => ({
+  plugins: [vue(), mode === 'development' && (await import('vite-plugin-vue-devtools')).default(), cloudflare()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+}));
